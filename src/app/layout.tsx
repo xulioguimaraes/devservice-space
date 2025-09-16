ab545b94fa919f3023e32e8fbb8547115c8b6512
Xulio <despairnatural@gmail.com>
2025-09-16 13:45:29 -0300

    GoogleTagManager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import GoogleTagManager, { GoogleTagManagerNoscript } from "@/components/GoogleTagManager";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,9 +27,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <GoogleTagManager />
+      </head>
       <body
         className={`${inter.variable} font-sans antialiased`}
       >
+        <GoogleTagManagerNoscript />
         {children}
       </body>
     </html>
